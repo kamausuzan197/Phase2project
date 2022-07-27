0ef68116bd71ca1f4702da24c33b9f032dfae79c
kamausuzan197 <kamaususan197@gmail.com>
2022-07-27 15:43:27 +0300

    adding home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h1>Home.</h1>
+        <h2>Welcome to Healthy living.</h2>
+        <h3>Ever wanted to lose weight and you had trable getting the write diet
+          <br/>Or probably you had trable with getting a recipe that can help you cook a dish you love.
+          <br/> Worry no more , Healthy living got you covered.
+        </h3>
+  
+    </div>
+  )
+}
+
+export default Home
