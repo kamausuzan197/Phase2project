c4e298ab57cdca03996caa910b37cd81708a0573
kamausuzan197 <kamaususan197@gmail.com>
2022-07-27 20:32:18 +0300

    deploy to vercel

---
+++ src/pages/RecipeItems.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Recipes from "./Recipes";
+
+export default function RecipeItem() {
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchData = async () => {
+    const response = await axios.get(
+      "https://boiling-hollows-58510.herokuapp.com/recipe"
+    );
+    setRecipes(response.data);
+  };
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      fetchData();
+      setLoading(false);
+    })();
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <div>
+          {recipes.map((recipe) => (
+            <Recipes key={recipe.id} recipe={recipe} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
